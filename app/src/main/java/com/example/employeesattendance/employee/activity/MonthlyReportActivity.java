0e9d9e3b1c37d538cae49a5fe9ed92e60186ced4
archirayan <archirayan41@gmail.com>
2018-02-09 12:41:44 +0530

    8/02/2018

---
+++ app/src/main/java/com/example/employeesattendance/employee/activity/MonthlyReportActivity.java
@@ -0,0 +1,15 @@
+package com.example.employeesattendance.employee.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.employeesattendance.R;
+
+public class MonthlyReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthly_report);
+    }
+}
--- app/src/main/java/com/example/employeesattendance/employee/fragmrnts/SalaryReportFragment.java
+++ app/src/main/java/com/example/employeesattendance/employee/fragmrnts/SalaryReportFragment.java
@@ -3,6 +3,7 @@ package com.example.employeesattendance.employee.fragmrnts;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,10 +22,7 @@ import java.util.Locale;
  */
 public class SalaryReportFragment extends Fragment implements View.OnClickListener {
 
-    private ImageView activity_calendar_previousmonth, activity_calendar_nextmonth, img_salary_back;
-    private TextView activity_calendar_monthname;
-    private CompactCalendarView admin_compactcalendar_view;
-    private SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMMM - yyyy", Locale.getDefault());
+    private RecyclerView recycler_view_month;
 
     public SalaryReportFragment() {
         // Required empty public constructor
@@ -36,38 +34,13 @@ public class SalaryReportFragment extends Fragment implements View.OnClickListen
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_salary_report, container, false);
 
-        activity_calendar_previousmonth = (ImageView) view.findViewById(R.id.activity_calendar_previousmonth);
-        activity_calendar_nextmonth = (ImageView) view.findViewById(R.id.activity_calendar_nextmonth);
-        admin_compactcalendar_view = (CompactCalendarView) view.findViewById(R.id.admin_compactcalendar_view);
+        recycler_view_month = view.findViewById(R.id.recycler_view_month);
 
-        activity_calendar_previousmonth.setOnClickListener(this);
-        activity_calendar_nextmonth.setOnClickListener(this);
-        activity_calendar_monthname = (TextView) view.findViewById(R.id.activity_calendar_monthname);
-        activity_calendar_monthname.setText(dateFormatForMonth.format(admin_compactcalendar_view.getFirstDayOfCurrentMonth()));
-
-        admin_compactcalendar_view.setListener(new CompactCalendarView.CompactCalendarViewListener() {
-            public void onDayClick(Date dateClicked) {
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            }
-
-            @Override
-            public void onMonthScroll(Date firstDayOfNewMonth) {
-                activity_calendar_monthname.setText(dateFormatForMonth.format(admin_compactcalendar_view.getFirstDayOfCurrentMonth()));
-            }
-        });
         return view;
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.activity_calendar_previousmonth:
-                admin_compactcalendar_view.showPreviousMonth();
-                break;
-            case R.id.activity_calendar_nextmonth:
-                admin_compactcalendar_view.showNextMonth();
-                break;
-        }
 
     }
 }
